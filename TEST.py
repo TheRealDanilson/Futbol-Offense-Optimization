4eccfc935d5605eb05dd718f1c58268c204d768e
Daniel Cardenas <dc862@cornell.edu>
2018-07-10 17:24:42 -0400

    Change graphics loop code to be better optimized

--- TEST.py
+++ TEST.py
@@ -28,6 +28,18 @@ x_max = FIELD_BOUNDS[1]
 y_min = FIELD_BOUNDS[2] 
 y_max = FIELD_BOUNDS[3]
 
+for player in players:
+    if type(player) is Offender:
+        sprite = factory.from_image(RESOURCES.get_path("redPlayer.png"))
+    else:           #What about ball?
+        sprite = factory.from_image(RESOURCES.get_path("bluePlayer.png"))
+    (x, y) = player.getPosition()
+    sprite.position = (floor((x + x_max)*10), floor((-y + y_max)*10)   ) #We need to fix this so that the origin is at the center of each image
+    player_sprites[player] = sprite
+    spriterenderer.render(sprite)
+    
+soccerball = factory.from_image(RESOURCES.get_path("ball.png"))
+
 running = True
 
 while running:
@@ -36,27 +48,24 @@ while running:
         if event.type == sdl2.SDL_QUIT:
             running = False
             break
+        
     spriterenderer.render(background)
     for player in players:
-        if type(player) is Offender:
-            sprite = factory.from_image(RESOURCES.get_path("redPlayer.png"))
-        else:           #What about ball?
-            sprite = factory.from_image(RESOURCES.get_path("bluePlayer.png"))
+        sprite = player_sprites[player]
         (x, y) = player.getPosition()
         sprite.position = (floor((x + x_max)*10), floor((-y + y_max)*10)   ) #We need to fix this so that the origin is at the center of each image
-        player_sprites[player] = sprite
         spriterenderer.render(sprite)
-    soccerball = factory.from_image(RESOURCES.get_path("ball.png"))
+        
     (x,y) = game.ball.getPosition()
     soccerball.position = (floor((x + x_max)*10), floor((-y + y_max)*10)   )
     spriterenderer.render(soccerball)
-    processor = sdl2.ext.TestEventProcessor()
+    #processor = sdl2.ext.TestEventProcessor()
     #processor.run(window)
     #window.refresh()
     game.update()
     sleep(0.05)    
     
-if __name__ == "__main__":
-    sys.exit(run())   
+#if __name__ == "__main__":
+#    sys.exit(run())   
 
 
