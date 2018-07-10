6e27eb311b894c56bc234cd39788d13487dc8500
Daniel Stabile <Cosmos@dhcp-hol-2092.eduroam.cornell.edu>
2018-07-10 17:33:43 -0400

    Fixed more of the sprites positioning

--- test.py
+++ test.py
@@ -1,1 +1,78 @@
-print(__file__)
+import sys
+import sdl2.ext
+from Game import Game
+from Player import *
+from constants import *
+from math import floor
+from Ball import *
+
+RESOURCES = sdl2.ext.Resources(__file__, "Images")
+
+sdl2.ext.init()
+
+window = sdl2.ext.Window("Futbol Offense Optimization", size=(910, 610))
+window.show()
+
+factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+background = factory.from_image(RESOURCES.get_path("soccerField.png"))
+
+game = Game()
+
+players = game.playerList()
+spriterenderer = factory.create_sprite_render_system(window)
+spriterenderer.render(background)
+
+player_sprites = {}
+x_min = FIELD_BOUNDS[0]
+x_max = FIELD_BOUNDS[1]
+y_min = FIELD_BOUNDS[2] 
+y_max = FIELD_BOUNDS[3]
+
+
+for player in players:
+    if type(player) is Offender:
+        sprite = factory.from_image(RESOURCES.get_path("redPlayer.png"))
+    else:           #What about ball?
+        sprite = factory.from_image(RESOURCES.get_path("bluePlayer.png"))
+    (x, y) = player.getPosition()
+    spriteXSize = sprite.size[0]
+    spriteYSize = sprite.size[1]
+    sprite.position = (floor((x + x_max)*10-spriteXSize), floor((-y + y_max)*10-spriteYSize)   ) #We need to fix this so that the origin is at the center of each image
+    player_sprites[player] = sprite
+    spriterenderer.render(sprite)
+    
+soccerball = factory.from_image(RESOURCES.get_path("ball.png"))
+ballXSize = soccerball.size[0]
+ballYSize = soccerball.size[1]
+
+running = True
+
+while running:
+    events = sdl2.ext.get_events()
+    for event in events:                #This for loop is not working
+        if event.type == sdl2.SDL_QUIT:
+            running = False
+            break
+        
+    spriterenderer.render(background)
+    for player in players:
+        sprite = player_sprites[player]
+        (x, y) = player.getPosition()
+        spriteXSize = sprite.size[0]
+        spriteYSize = sprite.size[1]
+        sprite.position = (floor((x + x_max)*10-spriteXSize), floor((-y + y_max)*10-spriteYSize)   )#We need to fix this so that the origin is at the center of each image
+        spriterenderer.render(sprite)
+        
+    (x,y) = game.ball.getPosition()
+    soccerball.position = (floor((x + x_max)*10-ballXSize), floor((-y + y_max)*10-ballYSize)   )
+    spriterenderer.render(soccerball)
+    #processor = sdl2.ext.TestEventProcessor()
+    #processor.run(window)
+    #window.refresh()
+    game.update()
+    sleep(0.05)    
+    
+#if __name__ == "__main__":
+#    sys.exit(run())   
+
+
