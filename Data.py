554175c0733b063ef2945dcdb928d14f4d41b2a2
Tony Stark <40695688+tonystark24@users.noreply.github.com>
2018-07-16 22:33:28 -0400

    A lot of changes
    Added: Recording of where/who successful goals were shot from, number of decisions to keep.
    
    I have not tested everything I've added with the goal data, but in the worst case scenario it's just a syntax error.
    Also changed all misspellings of receive -- I was spelling it wrong (recieve is wrong).

--- Data.py
+++ Data.py
@@ -4,34 +4,42 @@
 # moved by defenders.
 
 class Data:
-    """ 
+    """
+    game            Game object.
+    offenders       List of offenders.
+    defenders       List of defenders.
     ballDist        Total units of distance that the ball has traveled.
     ballDistAlone   Total units of distance the ball has traveled while not
                     being possessed by a player.
     ballDistHeld    Total distance the ball has traveled while being possessed
                     by a player.
-    ballTimeAlone   Total number of timesteps the ball has not been possessed by
+    ballTimeAlone   Total # timesteps the ball has not been possessed by
                     a player.
-    ballTimeHeld    Total number of timesteps the ball has been possessed by a
+    ballTimeHeld    Total # timesteps the ball has been possessed by a
                     player.
     whoDist         Dict of total units of distances moved by each player.
     whoDistAlone    Dict of total units of distances moved by each offender
                     while not possessing the ball.
     whoDistHeld     Dict of total units of distances moved by each offender
                     while possessing the ball.
-    whoTimeAlone    Dict of total number of timesteps the ball has not been in
+    whoTimeAlone    Dict of # of timesteps the ball has not been in
                     possession for each offender.
     whoTimeHeld     Dict of total number of timesteps the ball has been in
                     possession for each offender.
     Wins            Number of wins for the offending team.
     Losses          Number of losses for the offending team.
     Passes          Number of passes made by the offending team.
-    whoIntercepts   List of each defender's intercepts.
-    whoRecieves     Dict of each offender's number of recieves.
-    whereIntercepts 
-    whereRecieves
-    whohadBall      Last PLAYER that had the ball -- Excludes None
-    pastBall        Last possessor of the ball -- Includes None
+    Keeps           Dict of # of decisions to keep the ball by each offender.
+    whoIntercepts   Dict of each defender's intercepts.
+    whoReceives     Dict of each offender's number of receives.
+    whereIntercepts Dict of the locations of each defender's intercepts.
+    whereReceives   Dict of the locations of each offender's receives.
+    whoGoals        Dict of # of goals made for each offender.
+    whereGoals      Dict of locations of where the ball was shot into the goal
+                    for each offender.
+    whohadBall      Last possessor of the ball -- Excludes None.
+    pastBall        Last possessor of the ball -- Includes None.
+    wherehadBall    Last location of the last possessor -- Excludes None.
     """
     
     def __init__(self,game):
@@ -60,11 +68,16 @@ class Data:
         self.Losses = 0
         
         # Player-Ball Interactions
-        self.whoRecieves = {}
-        self.whereRecieves = {}
+        self.whoReceives = {}
+        self.whereReceives = {}
         self.whoIntercepts = {}
         self.whereIntercepts = {}
         self.Passes = 0
+        self.Keeps = {}
+        
+        # Goals
+        self.whoGoals = {}
+        self.whereGoals = {}
         
         # For-loops to initialize dictionaries
         for i in self.game.players:
@@ -74,13 +87,17 @@ class Data:
             self.whoDistHeld[i] = 0
             self.whoTimeAlone[i] = 0
             self.whoTimeHeld[i] = 0
-            self.whoRecieves[i] = 0
-            self.whereRecieves[i] = []
+            self.whoReceives[i] = 0
+            self.whereReceives[i] = []
+            self.Keeps[i] = 0
+            self.whoGoals[i] = 0
+            self.whereGoals[i] = []
         for i in self.defenders:
             self.whoIntercepts[i] = 0
-            self.whereIntercepts = []
-
+            self.whereIntercepts[i] = []
+        
         # Past Time-Step Helper Attributes
+        self.wherehadBall = tuple(self.game.players[1].getPosition())
         self.whohadBall = self.game.players[1]
         self.pastBall = self.game.players[1]
 
@@ -95,57 +112,68 @@ class Data:
         Float, total ball distance traveled.
         """
         return self.ballDist
+    
     def get_ballDistAlone(self):
         """
         Float, total ball distance traveled without a possessor.
         """
         return self.ballDistAlone
+    
     def get_ballDistHeld(self):
         """
         Float, total ball distance traveled with a possessor.
         """
         return self.ballDistHeld
+    
     def get_ballTimeAlone(self):
         """
         Int, total ball time without a possessor.
         """
         return self.ballTimeAlone
+
     def get_ballTimeHeld(self):
         """
         Int, total ball time with a possessor.
         """
         return self.ballTimeHeld
+    
     def get_whoDist(self):
         """
-        List of distances traveled, in order of players.
+        Dict of distances traveled by each player.
         """
         return self.whoDist.copy()
+    
     def get_whoDistAlone(self):
         """
-        List of distances traveled by offenders without the ball.
+        Dict of distances traveled by each offender without the ball.
         """
         return self.whoDistAlone.copy()
+    
     def get_whoDistHeld(self):
         """
-        List of distances traveled by offenders with the ball.
+        Dict of distances traveled by each offender with the ball.
         """
         return self.whoDistHeld.copy()
+    
     def get_whoTimeAlone(self):
         """
-        List of each offender's time without the ball.
+        Dict of each offender's time without the ball.
         """
         return self.whoTimeAlone.copy()
+    
     def get_whoTimeHeld(self):
         """
-        List of each offender's time with the ball.
+        Dict of each offender's time with the ball.
         """
         return self.whoTimeHeld.copy()
+    
     def get_WinLoss(self):
         """
-        List of ints, wins to losses.
+        Tuple of ints, wins to losses.
         """
-        wl = [self.Wins, self.Losses]
+        wl = (self.Wins, self.Losses)
         return wl.copy()
+    
     def get_Winrate(self):
         """
         Float, percent wins.
@@ -156,17 +184,55 @@ class Data:
             return (self.Wins/self.Losses)*100
         except ZeroDivisionError:
             return 100
+    
     def get_whoIntercepts(self):
         """
-        List of each Defender's number of intercepts.
+        Dict of each defender's number of intercepts.
         """
         return self.whoIntercepts.copy()
+
+    def get_whoReceives(self):
+        """
+        Dict of the number of each offender's receives.
+        """
+        return self.whoReceives.copy()
+    
+    def get_whereIntercepts(self):
+        """
+        Dict of the locations of each defender's intercepts.
+        """
+        return self.whereIntercepts.copy()
+
+    def get_whereReceives(self):
+        """
+        Dict of the locations of each offender's receives.
+        """
+        return self.whereReceives.copy()
+    
     def get_Passes(self):
         """
         Int, number of passes overall.
         """
         return self.Passes
 
+    def get_Keeps(self):
+        """
+        Dict of number of decisions to keep by each offender.
+        """
+        return self.Keeps.copy()
+    
+    def get_whoGoals(self):
+        """
+        Dict of the number of goals made by each player.
+        """
+        return self.whoGoals.copy()
+    
+    def get_whereGoals(self):
+        """
+        Dict of the location of where each offender shot a goal from.
+        """
+        return self.whereGoals.copy()
+    
 
     #-----------
     # While-Loop Methods
@@ -211,13 +277,7 @@ class Data:
     def offender_DistTime(self):
         """
         Adds on time and distance traveled for *each* offender either with
-        or without the ball.
-        
-        -- This is all assuming game.players list follows an offender-first
-        format with half offender and half defender; ex. in [a,b,c,d,e,f]: a,
-        b, and c are offenders, d, e, and f are defenders.
-        
-        Parameter game: Game object
+        or without the ball. Also deals with keeping.
         """
         for i in self.offenders:
             x_moved = abs(i.getPosition()[0] - i.getOldPosition()[0])
@@ -231,16 +291,15 @@ class Data:
                 
             if i.hasBall() and (self.pastBall == i):
                 self.whoDistHeld[i] += hyp
+                self.Keeps[i] += 1
             else:
                 self.whoDistAlone[i] += hyp
 
 
     def handle_WinLoss(self):
         """
-        Records a win or a loss for the offending team. Only intercepts count
-        as losses. This function handles interceptions as well.
-        
-        Parameter game: Game object
+        Records a win or a loss for the offending team. Interceptions and out
+        of bounds count as losses. Also deals with goals.
         """
         for i in self.defenders:
             if i.hasBall() and not self.whohadBall.hasBall():
@@ -249,6 +308,8 @@ class Data:
                 self.whereIntercepts[i] += [tuple(i.getPosition())]
         
         if self.game.ball.isGoal(): # Unrelated to above. This is for wins.
+            self.whereGoals[self.whohadBall] += [self.wherehadBall]
+            self.whoGoals[self.whohadBall] += 1
             self.Wins += 1
         elif self.game.ball.isOutBounds():
             self.Losses += 1
@@ -261,10 +322,10 @@ class Data:
         for i in self.offenders:
             if i.hasBall() and (i != self.pastBall):
                 self.Passes += 1
-                self.whoRecieves[i] += 1
-                self.whereRecieves[i] += [tuple(i.getPosition())]
+                self.whoReceives[i] += 1
+                self.whereReceives[i] += [tuple(i.getPosition())]
     
-    
+
     def handle_Past(self):
         """
         Handles remembering stuff from the past timestep, that I need.
@@ -277,6 +338,7 @@ class Data:
         for i in self.game.players:
             if i.hasBall():
                 self.whohadBall = i
+                self.wherehadBall = tuple(i.getPosition())
         
         if self.game.ball.getPossession() is None:
             self.pastBall = None
